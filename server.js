972d5d9bfd521b0906673aaf04421a036fc0605a
kashifnaseem50 <kashifnaseem506@yahoo.com>
2021-11-05 14:37:16 +0500

    abc

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello class'))
+app.get('/', (req, res) => res.send('Hello world'))
 
 var server = app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port " + server.address().port + "...");
